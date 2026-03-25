d72adbdda253f82e23c23b454def67d011cf5771
trippersea <trip.cave@gmail.com>
2026-03-25 13:38:08 -0400

    Updated Ragged and Bousquet Partners

--- featured-partners.js
+++ featured-partners.js
@@ -1,7 +1,15 @@
 // Shared featured partner configuration for browser + Node patch scripts
 (function (root) {
   const FEATURED_PARTNERS = {
-   
+   'ragged-mountain-resort': { bookingUrl: 'https://www.raggedmountainresort.com/tickets', 
+tagline: 'Indy Pass accepted · Book direct for best rates', 
+
+}, 
+
+'bousquet-ski-area': { bookingUrl: 'https://bousquetmountain.com/season-passes/', 
+tagline: 'Next Year Season Passes On Sale', 
+
+},
   };
 
   function getFeaturedPartner(resortId) {
